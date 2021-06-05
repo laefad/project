d770caa1b74e2bda2e043a204b6bd645713a4478
laefad <FreeFlightA@yandex.ru>
2021-06-05 19:04:15 +0300

    delete mail

--- src/main/java/com/company/CourseWork/config/SecurityConfig.java
+++ src/main/java/com/company/CourseWork/config/SecurityConfig.java
@@ -31,15 +31,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-
                     //ITEM CONTROLLER
                     .antMatchers("/item", "/item/get/**").permitAll()
                     .antMatchers("/item/create/**",
                             "/item/delete/**").hasRole("ADMIN")
-
                     //MAIN CONTROLLER
                     .antMatchers("/").permitAll()
-
                     //ORDER CONTROLLER
                     .antMatchers("/order/send/**",
                             "/order/takeBack/**",
@@ -49,27 +46,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                             "/order/change/**").hasRole("USER")
                     .antMatchers("/order/accept/**").hasRole("ADMIN")
                     .antMatchers("/order/**").hasAnyRole("USER", "ADMIN")
-
                     //STOCK CONTROLLER
                     .antMatchers("/stock",
                             "/stock/get/**").permitAll()
                     .antMatchers("/stock/add/**",
                             "/stock/delete/**",
                             "/stock/change/**").hasRole("ADMIN")
-
                     //USER CONTROLLER
                     .antMatchers("/user/**").hasAnyRole("USER", "ADMIN")
-
                     //ACCESS CONTROLLER
                     .antMatchers("/login/**",
                             "/registration/**",
                             "/fail",
                             "/addUser/**").permitAll()
-
                     //RESOURCES
                     .antMatchers("/css/**").permitAll()
                     .antMatchers("/images/**").permitAll()
-
                     .anyRequest().denyAll()
                 .and()
                 .formLogin()
